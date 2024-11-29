6a6d3c7da2b9238a2b9227fb4e23410633407c5d
nicolerrr1 <165748582+nicolerrr1@users.noreply.github.com>
2024-11-29 13:55:31 +0800

    reupload

---
+++ Laboratory Activities/Laboratory Activity 4 - 28-11-24/shapes/Square.java
@@ -0,0 +1,24 @@
+package shapes;
+
+public class Square implements Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 4 * side;
+    }
+
+    @Override
+    public void printShapeType() {
+        System.out.println("Square");
+    }
+}
---
+++ Laboratory Activities/Laboratory Activity 4 - 28-11-24/shapes/Triangle.java
@@ -0,0 +1,27 @@
+package shapes;
+
+public class Triangle implements Shape {
+    private double a, b, c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    public double getArea() {
+        double s = (a + b + c) / 2; // Semi-perimeter
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    @Override
+    public double getPerimeter() {
+        return a + b + c;
+    }
+
+    @Override
+    public void printShapeType() {
+        System.out.println("Triangle");
+    }
+}
